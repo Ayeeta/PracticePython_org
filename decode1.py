e662a1a228eb9ab0a1caf12c2bb55ac9c7f02d23
Ayeeta <elijate@gmail.com>
2018-05-07 14:49:42 +0300

    Decode NY times webpage https://www.nytimes.com

---
+++ decode1.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+# the URL of the NY Times website we want to parse
+base_url = 'https://www.nytimes.com'
+
+# the syntax (according to the documentation) for how to 
+# "load" a webpage through Python
+r = requests.get(base_url)
+
+# how to decode the text of the HTML of the NY Times homepage
+# website. r comes from the requests request above
+soup = BeautifulSoup(r.text, "html.parser")
+
+# find and loop through all elements on the page with the 
+# class name "story-heading"
+
+for story_heading in soup.find_all(class_ = "story-heading"):
+    # for the story headings that are links, print out the text
+    # and format it nicely
+    # for the others, take the contents out and format it nicely
+    if story_heading.a:
+        print(story_heading.a.text.replace("\n", " ").strip())
+    else:
+        print(story_heading.contents[0].strip())
